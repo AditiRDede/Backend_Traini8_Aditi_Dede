c3f90ca1ef6d050881a3ed13ea61a30d3159e05f
AditiRDede <aditi.dede@gmail.com>
2024-10-09 00:58:15 +0530

    Project Structure created

---
+++ TrainingCenterManagement/src/main/java/com/Traini8/TrainingCenterManagement/TrainingCenterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Traini8.TrainingCenterManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCenterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCenterManagementApplication.class, args);
+	}
+
+}
---
+++ TrainingCenterManagement/src/test/java/com/Traini8/TrainingCenterManagement/TrainingCenterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Traini8.TrainingCenterManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCenterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
